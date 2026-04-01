e6f426360aaa2fcfcecf6ab553a43edea52bb5d9
omieibih <owharcourt@gmail.com>
2026-04-01 12:56:13 -0500

    Added unitTest for AI feature

--- app.py
+++ app.py
@@ -57,6 +57,7 @@ def results():
     return render_template("results.html", meals=meals, ingredients=ingredients, budget=budget, error=None)
 
 
+# Run server
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
