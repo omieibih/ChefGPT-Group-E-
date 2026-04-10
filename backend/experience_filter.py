729ba794117cbc24e06aa21dfbce3964d2d786a4
Romeo Cortez <romeo.cortez@ou.edu>
2026-04-09 19:09:30 -0500

    changes for experience level

---
+++ backend/experience_filter.py
@@ -0,0 +1,22 @@
+# Experience Filter begin
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+#temp storage for experience
+
+user 
+beginner_experience()
+intermediate_experience()
+advanced_experience()
+
+def filter_experience(experience):
+    if experience == "Beginner"::
+        return beginner_experience()
+    elif experience == "Intermediate":
+        return intermediate_experience()
+    elif experience == "Advanced":
+        return advanced_experience()
+    else:
+        
